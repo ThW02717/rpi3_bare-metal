e84b2f30cad72472ccf2ffb94f313dc5223df5ca
Your Name <you@example.com>
2026-01-24 01:16:29 +0800

    feature: bring up 4 cores with per core stack and ordered hello

--- src/kernel.c
+++ src/kernel.c
@@ -1,14 +1,42 @@
 #include "uart.h"
+static volatile unsigned int uart_ready = 0;
+static volatile unsigned int turn = 0;
 
-void kernel_main(void)
+static void delay(unsigned int cnt)
 {
-	uart_init();
-    for(int i = 0; i < 5000000; i++) { 
-        asm volatile("nop"); 
-    }
-	uart_send_string("Hello, world!\r\n");
-
-	while (1) {
-		uart_send(uart_recv());
+	while (cnt--) {
+		asm volatile("nop");
 	}
+}
+// core_id in x0
+void kernel_main(unsigned long core_id)
+{
+	// core_00 doing Uart init 
+	if (core_id == 0) {
+		uart_init();
+		delay(5000000);
+		uart_ready = 1;
+	} else {
+		while (uart_ready == 0) {
+			asm volatile("nop");
+		}
+	}
+	// Print message in order
+	while (turn != core_id) {
+		asm volatile("nop");
+	}
+	uart_send_string("Hello, from processor ");
+    uart_send((char)('0' + core_id));
+    uart_send_string("\r\n");
+	turn++;
+	// Echo only for core0
+	if (core_id == 0) {
+		while(1) {
+			uart_send(uart_recv());
+		}
+	}
+	// other cores park here in echo stage
+    while (1) {
+        asm volatile("wfe");
+    }
 }
