95abac4c2149b61a7b4d4bd996d12488413a87e4
Your Name <you@example.com>
2026-01-23 17:20:34 +0800

    feature: uart setting and hello world

---
+++ src/kernel.c
@@ -0,0 +1,14 @@
+#include "mini_uart.h"
+
+void kernel_main(void)
+{
+	uart_init();
+    for(int i = 0; i < 5000000; i++) { 
+        asm volatile("nop"); 
+    }
+	uart_send_string("Hello, world!\r\n");
+
+	while (1) {
+		uart_send(uart_recv());
+	}
+}
